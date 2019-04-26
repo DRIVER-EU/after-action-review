32b5278905a1efe0c19d898042fb55bc1cfa7e4a
tobritzh <thomas.obritzhauser@frequentis.com>
2019-04-26 20:36:45 +0200

    missing AARConstants added

---
+++ aar-service/src/main/java/eu/driver/aar/service/constants/AARConstants.java
@@ -0,0 +1,8 @@
+package eu.driver.aar.service.constants;
+
+public interface AARConstants {
+
+	public static final String BACKUP_TYPE_SQL = "sql";
+	public static final String BACKUP_TYPE_CSV = "csv";
+	
+}
