43b08f20eb3aa9c949241482c81934d3d0c11ebb
Juergen Schoeffmann <juergen@schoeffmann.com>
2019-10-22 22:42:02 +0200

    #32: fixed CORS for use of sessions/cookies

--- aar-service/src/main/java/eu/driver/aar/service/filter/SimpleCORSFilter.java
+++ aar-service/src/main/java/eu/driver/aar/service/filter/SimpleCORSFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Component;
@@ -16,12 +17,17 @@ import org.springframework.stereotype.Component;
 public class SimpleCORSFilter implements Filter {
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+      HttpServletRequest request = (HttpServletRequest) req;
 		HttpServletResponse response = (HttpServletResponse) res;
-		response.setHeader("Access-Control-Allow-Origin", "*");
+		String referer = request.getHeader("Origin");
+		String origin = referer != null ? referer : "*";
+		System.err.println("SimpleCORSFilter.doFilter() " + origin);
+		response.setHeader("Access-Control-Allow-Origin", origin);
 		response.setHeader("Access-Control-Allow-Methods", "POST, PATCH, GET, OPTIONS, DELETE, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "Content-Type, cache-control, x-requested-with");
 		response.setHeader("Access-Control-Expose-Headers","Location");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("cache-control","no-store");
 		chain.doFilter(req, res);
 	}
